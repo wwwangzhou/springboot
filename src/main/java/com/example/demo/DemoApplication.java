05439b3ac689321d355427cf4a700679ce012bcf
Zhou Wang <zouwang@ucdavis.edu>
2021-11-04 21:57:42 -0700

    added student class

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,10 +1,13 @@
 package com.example.demo;
 
+import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.List;
 
 @SpringBootApplication
@@ -16,7 +19,9 @@ public class DemoApplication {
 	}
 
 	@GetMapping
-	public List<String> hello(){
-		return List.of("Hello World");
+	public List<Student> hello(){
+		return List.of(
+				new Student(1L,"Zhou", "Zhou@boring.com", LocalDate.of(2000, Month.NOVEMBER,5  ),12)
+		);
 	}
 }
