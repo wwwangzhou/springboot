55445077ff1b61e484de80da8975cfb3874243cc
Zhou Wang <zouwang@ucdavis.edu>
2021-11-04 22:15:20 -0700

    finished api layer and service layer

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,20 +1,27 @@
+// This is the API layer
+
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
 @RestController // make 'StudentController' class serve REST endpoints
 @RequestMapping(path = "api/v1/student")
 public class StudentController {
-    @GetMapping
-    public List<Student> getStudents (){
-        return List.of(
-                new Student(1L,"Zhou", "Zhou@boring.com", LocalDate.of(2000, Month.NOVEMBER,5  ),12)
-        );
+    private final StudentService studentService;
+
+    @Autowired
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
     }
+
+    @GetMapping
+    public List<Student> getStudents(){
+        return studentService.getStudents();
+    }
+
 }
